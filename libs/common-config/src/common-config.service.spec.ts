796bb532d2bad5d5500fc6d4ebd72208daee9a00
Hari Malam <malamharid@gmail.com>
2025-11-04 14:42:14 +0530

    fix test case

--- libs/common-config/src/common-config.service.spec.ts
+++ libs/common-config/src/common-config.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommonConfigService } from './common-config.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('CommonConfigService', () => {
   let service: CommonConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       providers: [CommonConfigService],
     }).compile();
 
